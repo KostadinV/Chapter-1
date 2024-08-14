c3e1140ca675294e983ee1095177c72382916d8e
Kostadin <stu1701261085@yni-plovdiv.bg>
2024-08-14 15:47:37 +0300

    asd

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -2,6 +2,6 @@
  * 6. Напишете програма, която изписва вашето име и фамилия на конзолата.
 */
 
-String name = "Kostadin Teodosiev";
+String name = "Kostadin Teodosieva";
 
 Console.WriteLine(name);
